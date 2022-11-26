c77e5f1bac99775d31e4dbafb1da44cdbf51a966
Josue Contreras <estavesyano@gmail.com>
2022-11-25 20:18:40 -0500

    Webpack Alias: webpack.config.js y @

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,13 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
-        extensions: ['.js']
+        extensions: ['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+        }
     },
     module: {
         rules: [
@@ -26,14 +32,13 @@ module.exports = {
             },
             {
                 test: /\.css|.styl$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
+                use: [MiniCssExtractPlugin.loader,
                     'css-loader',
-                    'stylus-loader',
+                    'stylus-loader'
                 ],
             },
             {
-                test: /\.png$/,
+                test: /\.png/,
                 type: 'asset/resource'
             },
             {
@@ -41,15 +46,15 @@ module.exports = {
                 use: {
                     loader: 'url-loader',
                     options: {
-                        limit: 10000,
-                        mimetype: 'application/font-woff',
-                        name: '[name].[contenthash].[ext]',
-                        outputPath: './assets/fonts/',
-                        publicPath: './assets/fonts/',
-                        esModule: false,
+                    limit: 10000,
+                    mimetype: "application/font-woff",
+                    name: "[name].[contenthash].[ext]",
+                    outputPath: "./assets/fonts/",
+                    publicPath: "../assets/fonts/",
+                    esModule: false,
                     },
                 }
-            },
+            }
         ]
     },
     plugins: [
@@ -64,8 +69,8 @@ module.exports = {
         new CopyPlugin({
             patterns: [
                 {
-                    from: path.resolve(__dirname, 'src', 'assets/images'),
-                    to: 'assets/images'
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images"
                 }
             ]
         })
