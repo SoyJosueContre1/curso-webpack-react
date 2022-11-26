e09c5ba957f3dce7422cc367089f4de49bd87129
Josue Contreras <estavesyano@gmail.com>
2022-11-25 19:16:47 -0500

    Loaders de fuentes: ulr-loader, file-loader y webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: 'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
         extensions: ['.js']
@@ -32,6 +33,20 @@ module.exports = {
             {
                 test: /\.png$/,
                 type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: 'application/font-woff',
+                        name: '[name].[ext]',
+                        outputPath: './assets/fonts/',
+                        publicPath: './assets/fonts/',
+                        esModule: false,
+                    },
+                }
             }
         ]
     },
